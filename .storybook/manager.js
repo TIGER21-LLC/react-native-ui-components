bf46d6819f5e688350cf9e7c58f06ce9a75c56ff
Sumeet Pareek <sumeet.pareek@tiger21.com>
2022-01-26 09:50:43 -0500

    Defaults to mobile viewport with layout changes like controls on the right

---
+++ .storybook/manager.js
@@ -0,0 +1,26 @@
+// .storybook/manager.js
+
+import { addons } from '@storybook/addons';
+
+addons.setConfig({
+  isFullscreen: false,
+  showNav: true,
+  showPanel: true,
+  panelPosition: 'right',
+  enableShortcuts: true,
+  isToolshown: true,
+  theme: undefined,
+  selectedPanel: undefined,
+  initialActive: 'sidebar',
+  sidebar: {
+    showRoots: true,
+    collapsedRoots: ['other'],
+  },
+  toolbar: {
+    title: { hidden: false, },
+    zoom: { hidden: true, },
+    eject: { hidden: true, },
+    copy: { hidden: true, },
+    fullscreen: { hidden: true, },
+  },
+});
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,10 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+// We use this to set the list of available viewports and the default viewport
+// See: 
+// https://www.npmjs.com/package/@storybook/addon-viewport
+// https://storybook.js.org/docs/react/essentials/viewport
+// https://github.com/storybookjs/storybook/blob/master/addons/viewport/src/defaults.ts
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -5,6 +12,10 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS, // newViewports would be an ViewportMap. (see below for examples)
+    defaultViewport: 'iphone6',
   },
   options: {
     storySort: {
